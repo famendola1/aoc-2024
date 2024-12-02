f334c404335647b22879a875932cb21dce81bd74
Fabio Amendola <fabio.amendola20@gmail.com>
2024-12-02 09:16:49 -0400

    day1

--- src/advent_of_code/day01.clj
+++ src/advent_of_code/day01.clj
@@ -4,9 +4,22 @@
 (defn part-1
   "Day 01 Part 1"
   [input]
-  "Implement this part")
+  (->> input
+       (u/to-lines)
+       (map u/parse-out-longs)
+       (u/transpose)
+       (map #(sort < %))
+       (apply map #(abs (- %1 %2)))
+       (apply +)))
 
 (defn part-2
   "Day 01 Part 2"
   [input]
-  "Implement this part")
+  input
+  (let [[group1 group2] (->> input
+                             (u/to-lines)
+                             (map u/parse-out-longs)
+                             (u/transpose))
+        freq (frequencies group2)]
+    (reduce + (map #(* %1 (or (freq %1) 0))                     
+                   group1))))
