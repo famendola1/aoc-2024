13d865980f19331f12bb96e98dd9246c5e975d43
Fabio Amendola <fabio.amendola20@gmail.com>
2024-12-22 14:17:36 -0500

    Day 22 Part 1

--- src/advent_of_code/day22.clj
+++ src/advent_of_code/day22.clj
@@ -1,10 +1,21 @@
 (ns advent-of-code.day22
   (:require [advent-of-code.utils :as u]))
 
+(defn- evolve-once [secret]
+  (let [step-1 (mod (bit-xor (* secret 64) secret) 16777216)
+        step-2 (mod (bit-xor (quot step-1 32) step-1) 16777216)]
+    (mod (bit-xor (* step-2 2048) step-2) 16777216)))
+
+(defn- evolve [times secret]
+  (reduce (fn [s _] (evolve-once s)) secret (range times)))
+
 (defn part-1
   "Day 22 Part 1"
   [input]
-  "Implement this part")
+  (->> input
+       (u/parse-out-longs)
+       (map (partial evolve 2000))
+       (apply +)))
 
 (defn part-2
   "Day 22 Part 2"
