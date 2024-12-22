3fb128e03f6e714832737c696cbf7f73d2461fc2
Fabio Amendola <fabio.amendola20@gmail.com>
2024-12-21 20:03:39 -0500

    Day 21 Part 1

--- src/advent_of_code/day21.clj
+++ src/advent_of_code/day21.clj
@@ -3,64 +3,94 @@
             [clojure.string :as string]))
 
 (def key-presses
-  {\A {\A ""
-       \< "v<<"
-       \^ "<"
-       \> "v"
-       \0 "<"
-       \1 "^<<"
-       \3 "^"
-       \4 "^^<<"
-       \9 "^^^"}
-   \< {\A ">>^"
-       \< ""
-       \v ">^"
-       \> ">>"
-       \^ ">^"}
-   \> {\A "^"
-       \> ""
-       \< "<<"
-       \v "<"
-       \^ "<^"}
-   \^ {\A ">"
-       \^ ""
-       \> "v>"
-       \< "v<"
-       \v "v"}
-   \0 {\2 "^"
-       \A ">"}
-   \1 {\7 "^^"}
-   \2 {\9 ">^^"}
-   \3 {\7 "<<^^"}   
-   \4 {\5 ">"}
-   \5 {\6 ">"}
-   \6 {\A "vv"}
-   \7 {\9 ">>"}
-   \8 {\0 "vvv"}
-   \9 {\A "vvv"
-       \8 "<"}})
+  {\A {\A "A"
+       \< "v<<A"
+       \^ "<A"
+       \v "v<A"
+       \> "vA"
+       \0 "<A"
+       \1 "^<<A"
+       \3 "^A"
+       \4 "^^<<A"
+       \5 "<^^A"
+       \6 "^^A"
+       \8 "<^^^A"
+       \9 "^^^A"}
+   \v {\A "^>A"
+       \v "A"
+       \< "<A"
+       \> ">A"
+       \^ "^A"}
+   \< {\A ">>^A"
+       \< "A"
+       \v ">A"
+       \> ">>A"
+       \^ ">^A"}
+   \> {\A "^A"
+       \> "A"
+       \< "<<A"
+       \v "<A"
+       \^ "^<A"}
+   \^ {\A ">A"
+       \^ "A"
+       \> "v>A"
+       \< "v<A"
+       \v "vA"}
+   \0 {\2 "^A"
+       \A ">A"}
+   \1 {\A ">>vA"
+       \7 "^^A"}
+   \2 {\A "v>A" 
+       \6 ">^A"
+       \9 "^^>A"}
+   \3 {\A "vA"
+       \4 "<<^A"
+       \7 "<<^^A"}
+   \4 {\1 "vA"
+       \5 ">A"}
+   \5 {\6 ">A"
+       \8 "^A"}
+   \6 {\A "vvA"
+       \7 "<<^A"}
+   \7 {\0 ">vvvA"
+       \9 ">>A"}
+   \8 {\0 "vvvA"
+       \2 "vvA"
+       \3 "vv>A"}
+   \9 {\A "vvvA"
+       \8 "<A"}})
 
 (defn- press-key-pad [keys]
-  (str (string/join (u/debug (map #(get-in key-presses %) (partition 2 1 keys))) "A") "A"))
+  (mapcat #(seq (get-in key-presses %))
+          (partition 2 1 (cons \A keys))))
 
 (defn- press-door-code [robots code]
-  (take (inc robots) (iterate press-key-pad (press-key-pad code))))
+  (reduce (fn [keys _] (press-key-pad keys))
+          code
+          (range (inc robots))))
 
 (defn- complexity [robots code]
-  (let [num (u/parse-out-longs code)]
-    (u/debug (press-door-code robots (str "A" code)))
-    0))
+  (let [num (first (u/parse-out-longs code))]
+    (* num (count (press-door-code robots code)))))
 
 (defn part-1
   "Day 21 Part 1"
   [input]
   (->> input
        (u/to-lines)
-       (map (partial complexity 0))
-       (apply +))  
-  "Implement this part")
+       (map (partial complexity 2))
+       (apply +)))
+
+(def press-key-pad-memo
+  (memoize
+   (fn [keys]
+     (mapcat #(seq (get-in key-presses %))
+             (partition 2 1 (cons \A keys))))))
 
 (defn part-2
-"Day 21 Part 2"
-[input]
-"Implement this part")
+  "Day 21 Part 2"
+  [input]
+  (->> input
+       (u/to-lines)
+       (map (partial complexity 25))
+       (apply +)))
